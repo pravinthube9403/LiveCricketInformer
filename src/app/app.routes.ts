03578e7d610e018718a283cfe27c93cdf418437d
pravinthube9403 <prvnthube@gmail.com>
2024-05-28 21:11:41 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { HomeComponent } from './pages/home/home.component';
+import path from 'path';
+import { LiveComponent } from './pages/live/live.component';
+import { PointTableComponent } from './pages/point-table/point-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path:'',
+      redirectTo:'/live',
+      pathMatch:'full'
+   },
+     {
+        path:"history",
+        component:HistoryComponent,
+        title:"Match | History | CrickInformer",
+     },
+     {
+        path:"live",
+        component:LiveComponent,
+        title:"Live Matches | CrickInformer",
+     },
+     {
+        path:"point-table",
+        component:PointTableComponent,
+        title:"Point Table | CrickInformer",
+     },
+];
---
+++ src/app/services/api-call/api-call.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-call',
+  standalone: true,
+  imports: [],
+  templateUrl: './api-call.component.html',
+  styleUrl: './api-call.component.css'
+})
+export class ApiCallComponent {
+
+}
